da36cc6be01437c92f4dfe6a0398d7fe7b228ee5
heiwa4126 <heiwa4126@gmail.com>
2024-04-15 10:25:09 +0900

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ const test = require("tape");
 const { hello } = require("../src/index");
 
 test("hello function returns the correct greeting", (t) => {
-  const result = hello();
-  t.equal(result, "Hello, NPM package!", "Should return the correct greeting");
-  t.end();
+	const result = hello();
+	t.equal(result, "Hello, NPM package!", "Should return the correct greeting");
+	t.end();
 });
