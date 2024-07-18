fb1b4c0ca3e5e5208a3ba9daad3f442ac28e0f55
heiwa4126 <heiwa4126@gmail.com>
2024-07-18 17:15:57 +0900

    [WIP] backup

--- scripts/clean-package.cjs
+++ scripts/clean-package.cjs
@@ -6,14 +6,22 @@ const backupPath = path.join(__dirname, "..", "package.json.bak");
 
 // オリジナルのpackage.jsonを読み込む
 const packageJson = require(packagePath);
+console.log(packageJson);
 
 // バックアップを作成
 fs.writeFileSync(backupPath, JSON.stringify(packageJson, null, 2));
 
 // 不要なスクリプトを削除
-delete packageJson.scripts.build;
-delete packageJson.scripts.test;
-// 他の不要なスクリプトも同様に削除
+// postpublishスクリプトを保存
+const postpublishScript = packageJson.scripts.postpublish;
+
+// scriptsオブジェクトを削除
+delete packageJson.scripts;
+
+// postpublishスクリプトだけを再追加
+packageJson.scripts = {
+	postpublish: postpublishScript,
+};
 
 // 更新されたpackage.jsonを書き込む
 fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
